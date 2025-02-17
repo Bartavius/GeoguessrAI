13e9b24b39d7796a4db694f5c5b2a4032d007296
Bartavius <lojanarungsiri.j@northeastern.edu>
2025-02-17 14:26:39 -0500

    added more locations for testing and single play

--- web/geoguessr-app/src/components/Streeview.tsx
+++ web/geoguessr-app/src/components/Streeview.tsx
@@ -1,33 +1,30 @@
 import "./Streetview.css";
 
-export default function StreetView({lat, lng}: {lat: number, lng: number}) {
+export default function StreetView({ lat, lng }: { lat: number; lng: number }) {
   const API_KEY = process.env.NEXT_PUBLIC_STREET_VIEW_API;
   // const lat = 42.345446;
   // const lng = -71.081856;
   return (
     <div className="street-view-container">
       <div className="street-view-box">
-      <div className="street-view-embed">
-      <div
-    className="absolute top-0 left-0 bg-red-900 w-full"
-    style={{ zIndex: 10000, height: 100}}
-  ></div>
-        <iframe
-          width="600"
-          height="450"
-          style={{ border: 0 }}
-          loading="lazy"
-          allowFullScreen
-          referrerPolicy="no-referrer-when-downgrade"
-          src={`https://www.google.com/maps/embed/v1/streetview?key=${API_KEY}
+        <div className="street-view-embed">
+          <div
+            className="absolute top-0 left-0 bg-red-900 w-full"
+            style={{ zIndex: 10000, height: 100 }}
+          ></div>
+          <iframe
+            width="600"
+            height="450"
+            style={{ border: 0 }}
+            loading="lazy"
+            allowFullScreen
+            referrerPolicy="no-referrer-when-downgrade"
+            src={`https://www.google.com/maps/embed/v1/streetview?key=${API_KEY}
       &location=${lat},${lng}
-      &heading=210
-      &pitch=10
-      &fov=35`}
-        ></iframe>
-      
-       
-      </div></div>
+      `}
+          ></iframe>
+        </div>
+      </div>
     </div>
   );
 }
